eb46b2cb4fd93cff5b1c57729d6698594c11c4c0
Vaishali-Tapaswi <52520564+Vaishali-Tapaswi@users.noreply.github.com>
2023-02-14 12:04:05 +0530

    Add files via upload

---
+++ src/main/java/demo/Calc.java
@@ -0,0 +1,7 @@
+package demo;
+
+public class Calc {
+	public int add(int i, int j) {
+		return i+j;
+	}
+}
---
+++ src/main/java/demo/HelloController.java
@@ -0,0 +1,13 @@
+package demo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "<h1>Greetings from Spring Boot!!</h1>";
+	}
+
+}
