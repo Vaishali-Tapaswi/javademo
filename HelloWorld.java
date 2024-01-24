c99fa9a517001a83e495d59fa5b875d3d5f28987
Vaishali-Tapaswi <52520564+Vaishali-Tapaswi@users.noreply.github.com>
2024-01-24 10:52:42 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+public class HelloWorld 
+{
+    public static void main(String []args)
+    {
+        System.out.println("My First Java Program at " + new Date() );
+    }
+};
